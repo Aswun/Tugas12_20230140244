c1a809e40de3cda61940db86f4cc0ed500c1c9a0
Aswun <aswinlutfian32@gmail.com>
2025-05-31 23:20:54 +0700

    up

---
+++ script.js
@@ -0,0 +1,25 @@
+function kirimData() {
+    var name = document.getElementById("name").value;
+    var nim = document.getElementById("nim").value;
+    var angkatan = document.getElementById("angkatan").value;
+    var tanggal = document.getElementById("tanggal").value;
+    var peminatan = document.querySelector("input[name=peminatan]:checked").value;
+    var alamat = document.getElementById("alamat").value;
+
+    // Format tanggal menjadi lebih mudah dibaca
+    var formattedDate = new Date(tanggal).toLocaleDateString('id-ID', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    alert(
+        "Name : " + name +
+        "\nNIM : " + nim +
+        "\nAngkatan : " + angkatan +
+        "\nTanggal Pendaftaran : " + formattedDate +
+        "\nPeminatan : " + peminatan +
+        "\nAlamat : " + alamat
+    );
+}
